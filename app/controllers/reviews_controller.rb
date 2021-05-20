52a466adfa105c79dc73a261b5c2ce3a3d916c7d
Liz Burton <liz.burton147@gmail.com>
2021-05-20 14:42:58 -0400

    update starter code to match instructions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,4 +5,24 @@ class ReviewsController < ApplicationController
     render json: reviews, include: :dog_house
   end
 
+  def show
+    review = Review.find_by(id: params[:id])
+    if review
+      render json: review
+    else
+      render json: { error: "Review not found" }, status: :not_found
+    end
+  end
+
+  def create
+    review = Review.create(review_params)
+    render json: review, status: :created
+  end
+
+  private
+
+  def review_params
+    params.permit(:username, :comment, :rating)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :dog_houses, only: [:show]
-  resources :reviews, only: [:index, :create, :update]
+  resources :reviews, only: [:show, :index, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
