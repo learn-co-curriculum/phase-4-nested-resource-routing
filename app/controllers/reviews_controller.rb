d008729f9eed2a0842d83e90573c01909169ef1e
Liz Burton <liz.burton147@gmail.com>
2021-05-20 15:06:41 -0400

    update starter code to match instructions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,7 +8,7 @@ class ReviewsController < ApplicationController
   def show
     review = Review.find_by(id: params[:id])
     if review
-      render json: review
+      render json: review, include: :dog_house
     else
       render json: { error: "Review not found" }, status: :not_found
     end
